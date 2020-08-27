fc57daf255e0d0768f3181844f0c76eb143b14f1
Dunka97 <1150311882@qq.com>
2020-08-27 21:05:14 +0800

    [Kiki]test : add test case:test voyageProfitFactor west-indies 10 and history include china and length 6,8,9.

--- test/rankTest.js
+++ test/rankTest.js
@@ -30,6 +30,117 @@ function generateHistoryAndLengthEquals4() {
   ];
 }
 
+function generateHistoryAndLengthEquals6() {
+  return  [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },{
+      zone: 'east-indies1',
+      profit: 5,
+    },{
+      zone: 'west-indies1',
+      profit: 15,
+    },
+  ];
+}
+
+function generateHistoryAndLengthEquals8() {
+  return  [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    }, {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'east-indies1',
+      profit: 5,
+    },{
+      zone: 'west-indies1',
+      profit: 15,
+    },{
+      zone: 'chinese',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa1',
+      profit: 7,
+    },
+  ];
+}
+
+function generateHistoryAndLengthEquals9() {
+  return  [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },{
+      zone: 'east-indies1',
+      profit: 5,
+    },{
+      zone: 'west-indies1',
+      profit: 15,
+    },{
+      zone: 'chinese',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa1',
+      profit: 7,
+    }, {
+      zone: 'west-africa2',
+      profit: 7,
+    },
+  ];
+}
+
+function generateHistoryAndLengthEquals4() {
+  return  [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+}
+
 
 rankTest('west-indies 10 and history include china and length 4', t => {
   const voyage = {
@@ -82,5 +193,32 @@ rankTest('test voyageProfitFactor west-indies 10 and history include china and l
     length: 10,
   };
   const expectedResult = voyageProfitFactor(voyage, generateHistoryAndLengthEquals4());
+  t.is(2, expectedResult);
+});
+
+rankTest('test voyageProfitFactor west-indies 15 and history include china and length 8', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 15,
+  };
+  const expectedResult = voyageProfitFactor(voyage, generateHistoryAndLengthEquals8());
+  t.is(1, expectedResult);
+});
+
+rankTest('test voyageProfitFactor west-indies 14 and history include china and length 9', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 14,
+  };
+  const expectedResult = voyageProfitFactor(voyage, generateHistoryAndLengthEquals9());
+  t.is(3, expectedResult);
+});
+
+rankTest('test voyageProfitFactor west-indies 10 and history include china and length 6', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const expectedResult = voyageProfitFactor(voyage, generateHistoryAndLengthEquals6());
   t.is(2, expectedResult);
 });
