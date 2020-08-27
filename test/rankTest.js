2123217e49e9d7a852fcc5f846a059c09b4675f3
Dunka97 <1150311882@qq.com>
2020-08-27 20:40:40 +0800

    [Kiki]test : add test case: west-indies 10 and history include china and length 4.

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,3 +1,5 @@
+import {rating} from "../src/rank";
+
 const rankTest = require('ava');
 
 rankTest('foo', t => {
@@ -7,4 +9,29 @@ rankTest('foo', t => {
 rankTest('bar', async t => {
   const bar = Promise.resolve('bar');
   t.is(await bar, 'bar');
+});
+
+rankTest('west-indies 10 and history include china and length 4', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+  const expectedResult = rating(voyage, history);
+  t.is('B',expectedResult);
 });
