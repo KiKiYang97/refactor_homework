6f335d83fc5bb3a42327ea97e2a4604f1e0ac1d2
Dunka97 <1150311882@qq.com>
2020-08-27 20:47:56 +0800

    [Kiki]test : add test case: west-indies 15 and history include china and length 4.

--- test/rankTest.js
+++ test/rankTest.js
@@ -49,3 +49,13 @@ rankTest('west-indies 14 and history include china and length 4', t => {
   t.is('B',expectedResult);
 });
 
+rankTest('west-indies 15 and history include china and length 4', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 15,
+  };
+  const expectedResult = rating(voyage, generateHistoryAndLengthEquals4());
+  t.is('B',expectedResult);
+});
+
+
