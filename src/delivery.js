6df567de3a2e573d0af798b717717fe086a91aa2
Dunka97 <1150311882@qq.com>
2020-08-29 00:31:29 +0800

    [Kiki]refactor : extract the consts.

--- src/delivery.js
+++ src/delivery.js
@@ -1,33 +1,41 @@
+const deliveryMAandCT = [
+  'MA',
+  'CT',
+];
+
+const deliveryNYandNH = [
+  'NY',
+  'NH',
+];
+
+const deliveryMAandCTandNY = [
+  'MA',
+  'CT',
+  'NY',
+];
+
+const deliveryNEandNH = [
+  'ME',
+  'NH',
+];
+
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
     let deliveryTime = 3;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
+    if (deliveryMAandCT.includes(anOrder.deliveryState)) {
       deliveryTime = 1;
     }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
+    else if (deliveryNYandNH.includes(anOrder.deliveryState)) {
       deliveryTime = 2;
     }
     return anOrder.placedOn.plusDays(1 + deliveryTime);
   }
   else {
     let deliveryTime = 4;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
+    if (deliveryMAandCTandNY.includes(anOrder.deliveryState)) {
       deliveryTime = 2;
     }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
+    else if (deliveryNEandNH.includes(anOrder.deliveryState)) {
       deliveryTime = 3;
     }
     return anOrder.placedOn.plusDays(2 + deliveryTime);
